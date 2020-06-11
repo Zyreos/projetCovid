59478315081df860d65847271f89023369290bfd
Eloisedp <44445016+Eloisedp@users.noreply.github.com>
2020-06-11 11:15:42 +0200

    Organize views

---
+++ resources/views/template_home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="Les Hydroalcooliques" />
+
+        <title>Les Hydroalcooliques</title>
+
+        @yield('css')
+
+    </head>
+    <body>
+
+    @yield('content')
+
+    </body>
+</html>
