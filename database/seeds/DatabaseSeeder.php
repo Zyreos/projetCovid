616bdfd7b0c6c06c0afed2f3e0ee81edf67e2e30
FannyDlfz <fanny.dlf.1808@gmail.com>
2020-06-11 16:35:30 +0200

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call(ArticlesTableSeeder::class);
         // $this->call(UserSeeder::class);
         $this->call(RoleTableSeeder::class);
+        $this->call(CategoryTableSeeder::class);
+
     }
 }
