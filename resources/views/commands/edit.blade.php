e57cf98950b5036e0551d243439187ea23812f41
Eloisedp <44445016+Eloisedp@users.noreply.github.com>
2020-06-16 18:16:46 +0200

    Begin view create() and edit() of Cmd

--- resources/views/commands/edit.blade.php
+++ resources/views/commands/edit.blade.php
@@ -1,1 +1,61 @@
-<?php
+@extends('template_home')
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/welcome.css') }}" />
+@endsection
+
+@section('content')
+
+    <section class="creation">
+        <h1 class="t1">Edition d'une commande</h1>
+
+        <form action="/commands/{{$command->id}}" method="POST" >
+            @csrf
+            {{ method_field('PATCH') }}
+            <div>
+                <input type="date" name="date_validation" placeholder="Date de validation">
+            </div>
+
+            <div>
+                <input type="number" name="total" placeholder="Total">
+            </div>
+
+            <label class="label">Statut</label>
+            <div class="select">
+                <select name="satus_id">
+                    @foreach($statuses as $status)
+                        <option value="{{ $status->id }}">{{ $status->name }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <label class="label"> Mode de livraison</label>
+            <div class="select">
+                <select name="delivery_id">
+                    @foreach($deliveries as $delivery)
+                        <option value="{{ $delivery->id }}">{{ $delivery->mode }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div>
+                <input type="text" name="{{$address->address1}}" placeholder="Addresse 1">
+            </div>
+            <div>
+                <input type="text" name="address" placeholder="Addresse 2">
+            </div>
+            <div>
+                <input type="number" name="postcode" placeholder="Code postal">
+            </div>
+            <div>
+                <input type="text" name="city" placeholder="Ville">
+            </div>
+            <div>
+                <input type="text" name="country" placeholder="Pays">
+            </div>
+
+            <button type="submit"> Ajouter </button>
+            <a href="/commands"> Annuler </a>
+        </form>
+    </section>
+
+@endsection
+
