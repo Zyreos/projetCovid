8d725e1d7e44f54d0c8b1ac185f6dca7131a942d
Eloisedp <44445016+Eloisedp@users.noreply.github.com>
2020-06-17 20:11:03 +0200

    Fix basket migration

---
+++ database/migrations/2020_06_17_180919_create_baskets_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBasketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('baskets', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer("total_order");
+            $table->timestamps();
+
+            $table->bigInteger('command_id')->unsigned();
+            $table->foreign('command_id')->references('id')->on('commands');
+            $table->bigInteger('article_id')->unsigned();
+            $table->foreign('article_id')->references('id')->on('articles');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+        Schema::dropIfExists('baskets');
+    }
+}
