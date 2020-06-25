ad796a4c9970c7ff8dbf23322d5ed035447386d9
FannyDlfz <fanny.dlf.1808@gmail.com>
2020-06-25 16:05:20 +0200

    starting front on loggin

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,20 +1,21 @@
-@extends('layouts.app')
+@extends('template_home')
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}" />
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
 
-                <div class="card-body">
+                <h1 class="title">Connexion à votre compte<h1/>
+
+                <section class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email">E-Mail</label>
 
-                            <div class="col-md-6">
+                            <div class="connexion-infos">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
@@ -65,9 +66,5 @@
                             </div>
                         </div>
                     </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+                </section>
 @endsection
