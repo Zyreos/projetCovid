4005ea895250cf5dd8ab24fe0dfe4e856cf404e2
Eloisedp <44445016+Eloisedp@users.noreply.github.com>
2020-06-28 18:17:05 +0200

    css and responsive basket view

--- resources/views/commands/basket.blade.php
+++ resources/views/commands/basket.blade.php
@@ -1,32 +1,57 @@
 @extends('template_home')
 
 @section('css')
-    <link rel="stylesheet" href="{{ asset('css/welcome.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/basket.css') }}" />
 @endsection
 
 @section('content')
 
-    <h1>Commande {{ $command->id }}</h1>
-    <h2>Utilisateur: {{ $user->name }}</h2>
+    <section class="global-body">
 
-    <ul>
+        <div class="basket">
 
-        @foreach($articles as $article)
-            <li>Article name: {{$article->name}}</li>
-            <li>Article id: {{$article->id}}</li>
-            <li>Article price: {{$article->price}}</li>
-            <li>Article quantity: {{$article->quantity}}</li>
+            @foreach($articles as $article)
+                <div class="article">
 
-            <form action="{{route('articles.updateQuantity', $article)}}" method="POST">
-                @csrf
-                <input id="quantity" name="quantity" value="{{$article->quantity}}" min="1">
-                <label for="quantity">Quantité</label>
-                <p>{{$article->price * $article->quantity}}</p>
-                <input type="hidden" id="total_price" name="total_price" value="{{$article->price * $article->quantity}}">
-                <button type="submit">Valider la quantité</button>
-            </form>
-        @endforeach
+                <div id="image1" class="article_image"></div>
 
-    </ul>
+                <div class="article-info">
 
+                    <h2 class="name">{{$article->name}}</h2>
+                    <p>{{$article->dimensions}}</p>
+                    <p class="price">Prix unitaire : {{$article->price}} €</p>
+
+                </div>
+
+
+                <form class="quantity" action="{{route('articles.updateQuantity', $article)}}" method="POST">
+                    @csrf
+                    <input id="quantity" name="quantity" value="{{$article->quantity}}" min="1">
+                    <label for="quantity"></label>
+
+                    <input type="hidden" id="total_price" name="total_price" value="{{$article->price * $article->quantity}}">
+                    <button type="submit">Valider</button>
+                </form>
+                    <p class="s-price">{{$article->price * $article->quantity}} €</p>
+
+                    <div class="delete">
+                    <img class="delete" src="/img/quit.png" alt="delete">
+                    </div>
+                </div>
+                <hr class="under-title">
+
+            @endforeach
+        </div>
+
+    <div class="recap-div">
+        <h2 class="sec-title">Récapitulatif de la commande</h2>
+        <hr class="recap-hr">
+        <div class="global-infos-div">
+            <p class="global-infos">TOTAL :</p><p class="global-infos">{{$command->total}} €</p>
+        </div>
+        <input type="hidden" name="total" value="{{$command->total}}">
+        <button class="submit-button" type="submit"> CONTINUER </button>
+    </div>
+
+    </section>
 @endsection
