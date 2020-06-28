0757a728fab5789d27c6762cd2b619376cde5a4a
Eloisedp <44445016+Eloisedp@users.noreply.github.com>
2020-06-28 14:55:43 +0200

    commands.show css

--- resources/views/commands/show.blade.php
+++ resources/views/commands/show.blade.php
@@ -1,7 +1,7 @@
 @extends('template_home')
 
 @section('css')
-    <link rel="stylesheet" href="{{ asset('css/welcome.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/commands_show.css') }}" />
 @endsection
 
 @section('content')
@@ -17,29 +17,29 @@
 
         <div class="main-infos">
 
-            <h1>Détail de la commande - {{ $command->id }}</h1>
+            <h1 class="title">Détail de la commande - {{ $command->id }}</h1>
 
             <div class="commands-infos">
 
                 <div class="articles">
 
-                    <h2>Montant :</h2>
+                    <h2 class="title-sec">Montant</h2>
 
                     @foreach($command->articles as $article)
-                        <div>
+                        <div class="list-montant">
                             <p>{{ $article->name }} x{{$article->quantity}}</p>
                             <p>{{$article->total_price}} €</p>
                         </div>
                     @endforeach
 
-                    <div>
+                    <div class="list-montant">
 
                         <p>Livraison</p>
-                        <p>{{$delivery->price}}</p>
+                        <p>{{$delivery->price}} €</p>
 
                     </div>
 
-                    <div>
+                    <div class="list-montant">
 
                         <p>TOTAL</p>
                         <p>{{$command->total}} €</p>
@@ -52,17 +52,17 @@
                     @foreach($command->addresses as $address)
 
                     @if($address->is_bill == 1)
-                            <h3>Adresse de facturation</h3>
+                            <h2 class="title-sec">Adresse de facturation</h2>
                     <p>{{$address->address1}}, {{$address->address2}}, {{$address->city}}, {{$address->postcode}}, {{$address->country}}</p>
 
                     @else($address->id_bill == 0)
-                    <h3>Adresse de livraison</h3>
+                    <h2 class="title-sec">Adresse de livraison</h2>
 
                         <p>{{$address->address1}}, {{$address->address2}}, {{$address->city}}, {{$address->postcode}}, {{$address->country}}</p>
                     @endif
 
                     @endforeach
-                    <h3>Date de validation</h3>
+                    <h2 class="title-sec">Date de validation</h2>
 
                     <p>{{$command->date_validation}}</p>
 
